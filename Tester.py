53c36069137a7c1e5a6864d80113ce6a918d5fac
Dustin Keller <inkydustin@gmail.com>
2023-06-27 18:27:40 -0400

    Added code for Q and Q/A ; needs fix

--- Tester.py
+++ Tester.py
@@ -1,10 +1,19 @@
 from Question import Question
+from QuestionAnswer import QuestionAnswer
 from langchain.llms import OpenAI
 from langchain import PromptTemplate, LLMChain
 import openai, os
 
-openai.organization = "org-64LJ5L34voPi4VOssqyzlQAk"
+# openai.organization = "org-64LJ5L34voPi4VOssqyzlQAk"
 
 llm = OpenAI(temperature=0.7)
-x = Question.problem("bicycles", "solving quadratic equations", llm)
-print(x)
+question = Question.problem("bicycles", "solving quadratic equations", llm)
+print(question)
+response = input()
+(correct, answer) = QuestionAnswer.check_answer(question, response, llm)
+if correct:
+    print("Correct!")
+else:
+    print("Incorrect")
+print(f"You answered: {response}")
+print(f"Correct answer: {answer}")
