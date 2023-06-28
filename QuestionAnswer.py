1a5f2c7035d45bad28fbaf47cd81ed5bde0ea8e0
Dustin Keller <inkydustin@gmail.com>
2023-06-27 20:07:06 -0400

    Adding QuestionAnswer

--- QuestionAnswer.py
+++ QuestionAnswer.py
@@ -8,7 +8,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class QuestionAnswer:
     def check_answer(question, response, llm):
         prompt = f"""
-        Given the below math question, return the answer to the question and only the answer (no labelling or units please) Only return the answer if it is positive.
+        Given the below math question, return the answer to the question and only the answer (no labelling or units please). Only return the answer if it is positive.
         {question}
         """
         answer = llm.predict(prompt)
